4a80b69f4898a4a6d568af55caae3541e5f089b6
JackSilver08 <tuantqtb01555@gmail.com>
2025-12-30 14:05:34 +0700

    Update features and clean repo structure

--- WebCam_Project/obj/Debug/net10.0/WebCam_Project.AssemblyInfo.cs
+++ WebCam_Project/obj/Debug/net10.0/WebCam_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCam_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f226a2fe116d9a99bcbce2fe2ed6f16404d489d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1325efd99fd108656015f2cede3f458f18bb5a7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCam_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCam_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
